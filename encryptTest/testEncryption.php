5073ad9472caed66f795b4a84bf41c3c8ca530b9
rgukhui <k.hui@rgu.ac.uk>
2016-04-11 15:41:23 +0100

    init commit

--- encryptTest/testEncryption.php
+++ encryptTest/testEncryption.php
@@ -1,19 +1,24 @@
 <?php
 header("Content-type: text/plain");
 
+$algorithms=openssl_get_cipher_methods();
+
 $data="hello world!";
 $method="aes";
 $password="my password";
 
-$cipherText=openssl_encrypt($data,$method,$password);
+foreach ($algorithms as $algo)
+    {
+    $cipherText=openssl_encrypt($data,$algo,$password);
+    echo "Algorithm: ".$algo."\n";
+    echo "Data: ".$data."\n";
+    echo "Password: ".$password."\n";
+    echo "Cipher text: ".$cipherText."\n";
 
-echo "Data: ".$data."\n";
-echo "Password: ".$password."\n";
-echo "Cipher text: ".$cipherText."\n";
+    $decryptedText=openssl_decrypt($cipherText,"aes",$password);
+    echo "Decrypted text: ".$decryptedText."\n\n";
 
-$decryptedText=openssl_decrypt($cipherText,"aes",$password);
-echo "Decrypted text: ".$decryptedText."\n";
-
+    }
 
 ?>
 
