1fe42801f7ab0aef21dc2fb7868fd808fc8e4328
rgukhui <k.hui@rgu.ac.uk>
2016-04-11 15:22:38 +0100

    init commit

--- hashTest/testHash.php
+++ hashTest/testHash.php
@@ -3,5 +3,12 @@ $algorithms=hash_algos();
 
 foreach ($algorithms as $algo)
     echo $algo."\n";
+
+$data="hello world!";
+echo "Data: ".$data."\n";
+
+$hash=hash("sha256",$data);
+
+echo "Hash: ".$hash."\n";
 ?>
 
