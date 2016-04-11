8ed81e5ba7196a21f0686f1f10f2c961b9a6db02
rgukhui <k.hui@rgu.ac.uk>
2016-04-11 15:28:22 +0100

    init commit

--- hashTest/testHash.php
+++ hashTest/testHash.php
@@ -6,10 +6,18 @@ $algorithms=hash_algos();
 $data="hello world!";
 echo "Data: ".$data."\n";
 
+$key="myKey";
+echo "Key: ".$key."\n";
+
+
 foreach ($algorithms as $algo)
     {
     $hash=hash($algo,$data);
     echo "Hash (".$algo."): ".$hash."\n";
+
+    $hmacHash=hash_hmac($algo,$data,$key);
+    echo "Hash (".$algo."): ".$hmacHash."\n";
+
     }
 ?>
 
